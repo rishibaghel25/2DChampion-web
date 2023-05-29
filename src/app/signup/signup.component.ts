425f17431e2e2ee4401cbee7c3dcf4a26744cc8a
shambhavi <shambhavivijay9@gmail.com>
2023-05-30 01:10:41 +0530

    signup page placing done!

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
   isLoginVisible: boolean = false;
+  isLeaderboardVisible: boolean = false;
+  isAboutVisible: boolean = false;
+  isGamesVisible: boolean = false;
   toggleLogin() {
     this.isLoginVisible = !this.isLoginVisible;
+    this.isLeaderboardVisible = false;
+    this.isGamesVisible = false;
+    this.isAboutVisible = false;
+
   }
 
 }
