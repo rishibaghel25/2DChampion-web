f59b1d1f3bb2b926449685d147a43f00c75c2249
Byte_Blaze <Byte@Blaze>
2023-06-22 21:20:05 -0400

    firebase started

---
+++ src/app/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEeXlgG0DY8772toIbXXvsmiZ9CbWTkUQ",
+  authDomain: "dchampion-4ce9f.firebaseapp.com",
+  projectId: "dchampion-4ce9f",
+  storageBucket: "dchampion-4ce9f.appspot.com",
+  messagingSenderId: "680322913494",
+  appId: "1:680322913494:web:31d067e3f3caf19d566577"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
